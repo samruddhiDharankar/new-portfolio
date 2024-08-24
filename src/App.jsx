31bfdc592c2317d443e19eaacc616580d45133c6
samruddhiDharankar <samruddhee.dharankar@gmail.com>
2024-08-24 12:37:47 -0700

    added google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,17 @@ import Projects from "./components/Projects/Projects";
 import Work from "./components/Work/Work";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import ReactGA from "react-ga4";
 
 const App = () => {
+  ReactGA.initialize("G-381M6BH03V");
+
+  ReactGA.send({
+    hitType: "pageview",
+    page: "/",
+    title: "Home",
+  });
+
   return (
     <div>
       <Navbar />
